abf70a8271d6b360b4d84eff886c702e96125305
Vladimir Slysar <g7au2ss@gmail.com>
2020-07-20 01:04:25 +0700

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
+class Example(object):
+    """docstring fo Example."""
+
+    def __init__(self, *args):
+        self.print_text()
+
+    def print_text(self):
+        print("In a hole in the ground there lived a method")
+
+
 def main():
     #write your code below this line
+    x = Example()
 
 if __name__ == '__main__':
     main()
